08429d4bfd2767ed4f2b5c24f731b3f5e85bcfb0
asamsonov <samsonov68rus@gmail.com>
2014-07-22 07:30:54 +0400

    1

--- js/vk.js
+++ js/vk.js
@@ -5,8 +5,8 @@ function doIt(){
         });
         */
 
-    VK.api("photos.get", function(data) {
-        
-
+    VK.api("users.get", function(data) {
+                        var photo_div = document.getElementById('user_photo');
+                        photo_div.innerHTML = "<img src="+data.response[0].photo_100+">";
     });
 }
