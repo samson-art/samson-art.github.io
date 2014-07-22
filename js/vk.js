b44a394d8d6e1fdde761131eb726f11855798110
asamsonov <samsonov68rus@gmail.com>
2014-07-22 06:53:19 +0400

    5

---
+++ js/vk.js
@@ -0,0 +1,12 @@
+function doIt(){
+        /*
+        VK.api('метод', {параметр: "значение"}, callback функция{
+        // работаем с полученными данными
+        });
+        */
+
+    VK.api("photos.get", function(data) {
+        
+
+    });
+}
