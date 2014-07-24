8d4a72613eb748cd3d8d971ad924cf09079257d1
asamsonov <samsonov68rus@gmail.com>
2014-07-24 04:58:39 +0400

    1

--- js/vk.js
+++ js/vk.js
@@ -10,7 +10,7 @@ function doIt(){
         photo_div.innerHTML = "<img src="+data.response[0].photo_100+">";
     });
     VK.api("friends.get", {fields: "photo_100"}, function(data) {
-        for (var i = Math.random()*data.response.length - 7, x=i+6; i < x; i++){
+        for (var i = Math.random()*data.response.length - 7, x=i+5; i < x; i++){
             var friends_div = document.getElementById('friends');
             friends_div.innerHTML += '<img class="friends" src="'+data.response[i].photo_100+'">';
         }
