38962a0d78e86e9489d801388b4a5592da96a22e
asamsonov <samsonov68rus@gmail.com>
2014-07-24 08:00:57 +0400

    1

--- js/vk.js
+++ js/vk.js
@@ -4,10 +4,13 @@ function doIt(){
         // работаем с полученными данными
         });
         */
-
+    var as = setInterval(function(){
+        var height = document.body.clientHeight > document.body.scrollHeight ? document.body.clientHeight : document.body.scrollHeight;
+        VK.callMethod("resizeWindow", 607, height);
+    }.bind(VK), 250);
     VK.api("users.get", {fields: "photo_100"},function(data) {
         var photo_div = document.getElementById('user_photo');
-        photo_div.innerHTML = "<img src="+data.response[0].photo_100+">";
+        photo_div.innerHTML = "<img class='photo' src="+data.response[0].photo_100+">";
     });
     VK.api("friends.get", {fields: "photo_100"}, function(data) {
         for (var i = (Math.random()*data.response.length - 7) | 0, x=i+5; i < x; i++){
@@ -15,18 +18,17 @@ function doIt(){
             create_img(data.response[i]);
         }
     });
-}
-function create_img(user) {
-    var img = document.createElement('img');
-    img.onclick = function(){
-        VK.callMethod('showInviteBox');
-    }.bind(VK);
-    img.src = user.photo_100;
-    img.title = user.first_name + ' ' + user.last_name;
-    img.className = 'photo';
-    document.getElementById('friends').appendChild(img);
-    img.onload = function(){
-        var height=(document.body.scrollHeight > document.body.offsetHeight)?document.body.scrollHeight:document.body.offsetHeight;
-        VK.callMethod('resizeWindow', 607, height);
-    }.bind(VK);
+    function create_img(user) {
+        var img = document.createElement('img');
+        img.onclick = function(){
+            VK.callMethod('showInviteBox');
+        }.bind(VK);
+        img.src = user.photo_100;
+        img.title = user.first_name + ' ' + user.last_name;
+        img.className = 'photo';
+        document.getElementById('friends').appendChild(img);
+        img.onload = function(){
+            clearInterval(as);
+        }.bind(this);
+    }
 }
