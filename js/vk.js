eb7671364dfeb7c580e75b0aed2e32fd778ed7fc
asamsonov <samsonov68rus@gmail.com>
2014-07-24 08:16:03 +0400

    1

--- js/vk.js
+++ js/vk.js
@@ -4,21 +4,20 @@ function doIt(){
         // работаем с полученными данными
         });
         */
-    var as = setInterval(function(){
-        var height = document.body.clientHeight > document.body.scrollHeight ? document.body.clientHeight : document.body.scrollHeight;
-        VK.callMethod("resizeWindow", 607, height);
-    }.bind(VK), 250);
+    this.avtosize = function() {
+            var height = document.body.clientHeight > document.body.scrollHeight ? document.body.clientHeight : document.body.scrollHeight;
+            VK.callMethod("resizeWindow", 607, height);
+    }().bind(VK);
     VK.api("users.get", {fields: "photo_100"},function(data) {
         var photo_div = document.getElementById('user_photo');
         photo_div.innerHTML = "<img class='photo' src="+data.response[0].photo_100+">";
     });
     VK.api("friends.get", {fields: "photo_100"}, function(data) {
         for (var i = (Math.random()*data.response.length - 7) | 0, x=i+5; i < x; i++){
-            var friends_div = document.getElementById('friends');
-            create_img(data.response[i]);
+            this.create_img(data.response[i]);
         }
-    });
-    function create_img(user) {
+    }.bind(this));
+    this.create_img = function (user) {
         var img = document.createElement('img');
         img.onclick = function(){
             VK.callMethod('showInviteBox');
@@ -26,9 +25,9 @@ function doIt(){
         img.src = user.photo_100;
         img.title = user.first_name + ' ' + user.last_name;
         img.className = 'photo';
-        document.getElementById('friends').appendChild(img);
-        img.onload = function(){
-            clearInterval(as);
+        img.onload = function () {
+            this.avtosize();
         }.bind(this);
-    }
-}
+        document.getElementById('friends').appendChild(img);
+    }.bind(this);
+ }
